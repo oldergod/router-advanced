7b2804c4776ab257e9255b4bb51846b4d86c64f8
benoit <oldering@gmail.com>
2016-07-02 14:24:30 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET=ui-element-samples
+
+[ -e $TARGET ] && rm -rf $TARGET
+
+git clone https://github.com/GoogleChrome/ui-element-samples.git &&
+scp -r $TARGET/router-advanced/* .
+
+CURRENT_HASH=$(cd $TARGET;git rev-parse --short HEAD)
+
+cd .. && rm -rf $TARGET
+git diff-index --quiet HEAD -- && exit("no changes")
+
+git add --all . &&
+git commit -m "updated the advanced router to $CURRENT_HASH"
+
+echo "done, should only git push origin gh-pages"
